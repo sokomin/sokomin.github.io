5a084ce59d0ff324c30ece36589e9e444e350998
Sokomin <sokoranominsyu@gmail.com>
2019-01-19 18:39:44 +0900

    アイテム一覧さらに追加

---
+++ item/design/image/item/rename - コピー.sh
@@ -0,0 +1,15 @@
+﻿if [ $# -lt 3 ]
+then
+	echo "変更前の文字列, 変更後の文字列, 変更したいファイル名を入力して下さい."
+	exit 1
+fi
+
+args=$*
+extra_args="$1 $2"
+files=${args#$extra_args}
+
+for f in $files
+do
+	mv $f ${f/$1/$2}
+done
+            
---
+++ item/design/image/item/rename.bat
@@ -0,0 +1,11 @@
+﻿echo off
+for %%f in ( * ) do call :sub "%%f"
+exit /b
+
+:sub
+set fname=%1
+set fname=%fname:(=【%
+set fname=%fname:)=】%
+set fname=%fname:IcomItem_=0%
+ren %1 %fname%
+goto :EOF
