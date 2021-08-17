48cc2d94c4665cdb33e4ac7a7a32cb1bf89cb1d6
sokomin <sokoranominsyu@gmail.com>
2021-08-18 08:42:53 +0900

    町対応（続き）

--- map/database/mobdb257.js
+++ map/database/mobdb257.js
@@ -1,23 +1,27 @@
 MobData = {
-257: [{ id: "0",name:"旅館主人",type:1,inid:0,repop:120,isptboss:0,posx:33.80,posy:14.00, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "1",name:"協会管理魔法師",type:14,inid:1,repop:120,isptboss:0,posx:64.05,posy:48.40, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "2",name:"住民",type:1,inid:3,repop:120,isptboss:0,posx:46.40,posy:20.35, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "3",name:"ナルブ",type:1,inid:2,repop:120,isptboss:0,posx:30.00,posy:24.05, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "4",name:"協会テレポーター",type:35,inid:4,repop:120,isptboss:0,posx:57.95,posy:48.75, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "5",name:"支部長ランベル",type:35,inid:5,repop:120,isptboss:0,posx:115.45,posy:41.25, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "6",name:"ストレンジ調査官",type:37,inid:13,repop:120,isptboss:0,posx:93.45,posy:72.95, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "7",name:"装備商人",type:3,inid:7,repop:120,isptboss:0,posx:107.70,posy:23.55, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "8",name:"雑貨商人",type:5,inid:8,repop:120,isptboss:0,posx:94.85,posy:17.15, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "9",name:"協会物資管理人",type:1,inid:9,repop:120,isptboss:0,posx:77.85,posy:25.95, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "10",name:"治療師",type:16,inid:10,repop:120,isptboss:0,posx:67.75,posy:33.05, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "13",name:"協会事務官",type:35,inid:11,repop:120,isptboss:0,posx:91.95,posy:38.85, lv_min:0, lv_max:0, is_npc: false  },
-]};
+    257: [ {"id":"0","inid":0,"type":1,"name":"旅館主人","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":10.56,"real_posy":8.75,"posx":33.8,"posy":14},
+    {"id":"1","inid":1,"type":14,"name":"協会管理魔法師","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":20.02,"real_posy":30.25,"posx":64.05,"posy":48.4},
+    {"id":"2","inid":3,"type":1,"name":"住民","repop":120,"id_area":3,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":14.5,"real_posy":12.72,"posx":46.4,"posy":20.35},
+    {"id":"3","inid":2,"type":1,"name":"ナルブ","repop":120,"id_area":2,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":9.38,"real_posy":15.03,"posx":30,"posy":24.05},
+    {"id":"4","inid":4,"type":35,"name":"協会テレポーター","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":18.11,"real_posy":30.47,"posx":57.95,"posy":48.75},
+    {"id":"5","inid":5,"type":35,"name":"支部長ランベル","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":36.08,"real_posy":25.78,"posx":115.45,"posy":41.25},
+    {"id":"6","inid":13,"type":37,"name":"ストレンジ調査官","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":29.2,"real_posy":45.59,"posx":93.45,"posy":72.95},
+    {"id":"7","inid":7,"type":3,"name":"装備商人","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":33.66,"real_posy":14.72,"posx":107.7,"posy":23.55},
+    {"id":"8","inid":8,"type":5,"name":"雑貨商人","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":29.64,"real_posy":10.72,"posx":94.85,"posy":17.15},
+    {"id":"9","inid":9,"type":1,"name":"協会物資管理人","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":24.33,"real_posy":16.22,"posx":77.85,"posy":25.95},
+    {"id":"10","inid":10,"type":16,"name":"治療師","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":21.17,"real_posy":20.66,"posx":67.75,"posy":33.05},
+    {"id":"11","inid":14,"type":1,"name":"協会調査官","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":23.92,"real_posy":46.75,"posx":76.55,"posy":74.8},
+    {"id":"13","inid":11,"type":35,"name":"協会事務官","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":28.73,"real_posy":24.28,"posx":91.95,"posy":38.85},
+    ]
+};
+
 AreaData = {
-257: [{ id: "0",name:"none",area_name:"_필드 전체",type:0,posx:0.00,posx2:0.00,access_map:"none",posy:0.00,posy2:0.00,is_secret:0},
-{ id: "1",name:"none",area_name:"_화면",type:0,posx:0.00,posx2:0.00,access_map:"none",posy:0.00,posy2:0.00,is_secret:0},
-{ id: "2",name:"none",area_name:"Area 1",type:4,posx:22.70,posx2:36.55,access_map:"none",posy:18.80,posy2:30.10,is_secret:0},
-{ id: "3",name:"none",area_name:"Area 2",type:4,posx:40.85,posx2:49.40,access_map:"none",posy:15.50,posy2:23.45,is_secret:0},
-{ id: "4",name:"魔法都市スマグ",area_name:"모험가 협회 출구",type:3,posx:29.15,posx2:33.65,access_map:"[214]T11.rmd",posy:59.55,posy2:62.55,is_secret:0},
-{ id: "5",name:"none",area_name:"시작 영역",type:5,posx:73.75,posx2:79.65,access_map:"none",posy:45.55,posy2:50.25,is_secret:0},
-{ id: "6",name:"冒険家協会バー",area_name:"Area 5",type:3,posx:30.70,posx2:37.05,access_map:"[544] T01_I01_B01.rmd",posy:38.80,posy2:43.40,is_secret:0},
-]};
+    257: [ {"id":"0","type":0,"name":"_필드 전체","access_map":"","is_secret":0,"real_posx":0,"real_posx2":0,"real_posy":0,"real_posy2":0,"posx":0,"posx2":0,"posy":0,"posy2":0},
+    {"id":"1","type":0,"name":"_화면","access_map":"","is_secret":0,"real_posx":0,"real_posx2":0,"real_posy":0,"real_posy2":0,"posx":0,"posx2":0,"posy":0,"posy2":0},
+    {"id":"2","type":4,"name":"Area 1","access_map":"","is_secret":0,"real_posx":7.09,"real_posx2":11.42,"real_posy":11.75,"real_posy2":18.81,"posx":22.7,"posx2":36.55,"posy":18.8,"posy2":30.1},
+    {"id":"3","type":4,"name":"Area 2","access_map":"","is_secret":0,"real_posx":12.77,"real_posx2":15.44,"real_posy":9.69,"real_posy2":14.66,"posx":40.85,"posx2":49.4,"posy":15.5,"posy2":23.45},
+    {"id":"4","type":3,"name":"魔法都市スマグ","access_map":"[214]T11.rmd","is_secret":0,"real_posx":9.11,"real_posx2":10.52,"real_posy":37.22,"real_posy2":39.09,"posx":29.15,"posx2":33.65,"posy":59.55,"posy2":62.55},
+    {"id":"5","type":5,"name":"시작 영역","access_map":"","is_secret":0,"real_posx":23.05,"real_posx2":24.89,"real_posy":28.47,"real_posy2":31.41,"posx":73.75,"posx2":79.65,"posy":45.55,"posy2":50.25},
+    {"id":"6","type":3,"name":"冒険家協会バー","access_map":"[544] T01_I01_B01.rmd","is_secret":0,"real_posx":9.59,"real_posx2":11.58,"real_posy":24.25,"real_posy2":27.13,"posx":30.7,"posx2":37.05,"posy":38.8,"posy2":43.4},
+    ]
+};
--- map/database/mobdb480.js
+++ map/database/mobdb480.js
@@ -1,20 +1,26 @@
 MobData = {
-480: [{ id: "0",name:"船員",type:15,inid:0,repop:120,isptboss:0,posx:135.06,posy:34.69, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "1",name:"アンジー",type:13,inid:1,repop:120,isptboss:0,posx:12.63,posy:66.41, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "2",name:"ビルド・デーズ",type:1,inid:2,repop:120,isptboss:0,posx:93.25,posy:52.13, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "3",name:"ダドリー",type:1,inid:3,repop:120,isptboss:0,posx:75.25,posy:73.75, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "4",name:"冒険家",type:1,inid:4,repop:120,isptboss:0,posx:79.56,posy:76.88, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "5",name:"冒険家",type:1,inid:5,repop:120,isptboss:0,posx:75.03,posy:78.09, lv_min:0, lv_max:0, is_npc: false  },
-{ id: "6",name:"冒険家",type:1,inid:6,repop:120,isptboss:0,posx:71.03,posy:76.31, lv_min:0, lv_max:0, is_npc: false  },
-]};
+    480: [ {"id":"0","inid":0,"type":15,"name":"船員","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":67.53,"real_posy":34.69,"posx":236.36,"posy":60.7},
+    {"id":"1","inid":1,"type":13,"name":"アンジー","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":true,"real_posx":6.31,"real_posy":66.41,"posx":22.09,"posy":116.21},
+    {"id":"2","inid":2,"type":1,"name":"ビルド・デーズ","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":46.63,"real_posy":52.13,"posx":163.19,"posy":91.22},
+    {"id":"3","inid":3,"type":1,"name":"ダドリー","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":37.63,"real_posy":73.75,"posx":131.69,"posy":129.06},
+    {"id":"4","inid":4,"type":1,"name":"冒険家","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":39.78,"real_posy":76.88,"posx":139.23,"posy":134.53},
+    {"id":"5","inid":5,"type":1,"name":"冒険家","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":37.52,"real_posy":78.09,"posx":131.3,"posy":136.66},
+    {"id":"6","inid":6,"type":1,"name":"冒険家","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":35.52,"real_posy":76.31,"posx":124.3,"posy":133.55},
+    {"id":"7","inid":7,"type":1,"name":"超越の導師バラン","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":32.42,"real_posy":38.84,"posx":113.48,"posy":67.98},
+    {"id":"8","inid":8,"type":1,"name":"ヒュイ","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":23.63,"real_posy":34.31,"posx":82.69,"posy":60.05},
+    ]
+};
+
 AreaData = {
-480: [{ id: "0",name:"none",area_name:"_필드 전체",type:0,posx:0.00,posx2:0.00,access_map:"none",posy:0.00,posy2:0.00,is_secret:0},
-{ id: "1",name:"none",area_name:"_화면",type:0,posx:0.00,posx2:0.00,access_map:"none",posy:0.00,posy2:0.00,is_secret:0},
-{ id: "2",name:"none",area_name:"[입구] 용사들의 무덤",type:0,posx:12.06,posx2:16.63,access_map:"none",posy:48.03,posy2:50.66,is_secret:0},
-{ id: "3",name:"ボルティッシュ銀行",area_name:"[입구] 작은은행",type:3,posx:48.63,posx2:52.00,access_map:"[481]T19_Bank01.rmd",posy:70.28,posy2:72.03,is_secret:0},
-{ id: "4",name:"ボルティッシュ旅館 / 酒場",area_name:"[입구] 여관겸 주점, 잡화점",type:3,posx:98.00,posx2:101.44,access_map:"[484]T19_I01.rmd",posy:46.25,posy2:48.19,is_secret:0},
-{ id: "6",name:"ボルティッシュ教会",area_name:"[이동] 교회",type:3,posx:28.63,posx2:31.81,access_map:"[482]T19_C01.rmd",posy:30.94,posy2:32.69,is_secret:0},
-{ id: "7",name:"ボルティッシュ武器店",area_name:"[이동] 무기점",type:3,posx:64.59,posx2:67.78,access_map:"[483]T19_W01.rmd",posy:59.19,posy2:60.91,is_secret:0},
-{ id: "8",name:"none",area_name:"시작 영역",type:5,posx:121.88,posx2:127.91,access_map:"none",posy:34.22,posy2:38.84,is_secret:0},
-{ id: "9",name:"幻影の谷 / シトロンリバー上流",area_name:"M27로",type:3,posx:27.81,posx2:33.44,access_map:"[486]M27.rmd",posy:91.09,posy2:94.78,is_secret:0},
-]};
+    480: [ {"id":"0","type":0,"name":"_필드 전체","access_map":"","is_secret":0,"real_posx":0,"real_posx2":0,"real_posy":0,"real_posy2":0,"posx":0,"posx2":0,"posy":0,"posy2":0},
+    {"id":"1","type":0,"name":"_화면","access_map":"","is_secret":0,"real_posx":0,"real_posx2":0,"real_posy":0,"real_posy2":0,"posx":0,"posx2":0,"posy":0,"posy2":0},
+    {"id":"2","type":0,"name":"[입구] 용사들의 무덤","access_map":"","is_secret":0,"real_posx":6.03,"real_posx2":8.31,"real_posy":48.03,"real_posy2":50.66,"posx":21.11,"posx2":29.09,"posy":84.05,"posy2":88.65},
+    {"id":"3","type":3,"name":"ボルティッシュ銀行","access_map":"[481]T19_Bank01.rmd","is_secret":0,"real_posx":24.31,"real_posx2":26,"real_posy":70.28,"real_posy2":72.03,"posx":85.09,"posx2":91,"posy":122.99,"posy2":126.05},
+    {"id":"4","type":3,"name":"枯れ井戸","access_map":"[017]G16_D01_B01.rmd","is_secret":0,"real_posx":49,"real_posx2":50.72,"real_posy":46.25,"real_posy2":48.19,"posx":171.5,"posx2":177.52,"posy":80.94,"posy2":84.33},
+    {"id":"5","type":0,"name":"초월 스킬","access_map":"","is_secret":0,"real_posx":30.16,"real_posx2":34.8,"real_posy":34.53,"real_posy2":41.53,"posx":105.55,"posx2":121.79,"posy":60.43,"posy2":72.68},
+    {"id":"6","type":3,"name":"ボルティッシュ教会","access_map":"[482]T19_C01.rmd","is_secret":0,"real_posx":14.31,"real_posx2":15.91,"real_posy":30.94,"real_posy2":32.69,"posx":50.09,"posx2":55.67,"posy":54.14,"posy2":57.2},
+    {"id":"7","type":3,"name":"ボルティッシュ武器店","access_map":"[483]T19_W01.rmd","is_secret":0,"real_posx":32.3,"real_posx2":33.89,"real_posy":59.19,"real_posy2":60.91,"posx":113.04,"posx2":118.62,"posy":103.58,"posy2":106.59},
+    {"id":"8","type":5,"name":"시작 영역","access_map":"","is_secret":0,"real_posx":60.94,"real_posx2":63.95,"real_posy":34.22,"real_posy2":38.84,"posx":213.28,"posx2":223.84,"posy":59.88,"posy2":67.98},
+    {"id":"9","type":3,"name":"幻影の谷 / シトロンリバー上流","access_map":"[486]M27.rmd","is_secret":0,"real_posx":13.91,"real_posx2":16.72,"real_posy":91.09,"real_posy2":94.78,"posx":48.67,"posx2":58.52,"posy":159.41,"posy2":165.87},
+    ]
+};
