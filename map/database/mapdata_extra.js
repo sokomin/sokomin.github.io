2a8094c467b412d323ff4dbfd02d516e6e7047dc
sokomin <sokoranominsyu@gmail.com>
2021-08-22 16:40:33 +0900

    秘密インデックス追記

--- map/database/mapdata_extra.js
+++ map/database/mapdata_extra.js
@@ -1,12 +1,17 @@
 ﻿//イレギュラーすぎて対応しきれん情報とかは全部ここに直書きしといてください
 //配列テキストが改行されてそのまんま表示されます。
 MapExtraInfoList = {
+    286: ["秘密ダンジョン「<a href='../secret/secretdgn500-550.html'>呪いを受けたミズナ洞窟の隠された洞窟</a>」"],
     293: ["秘密ダンジョン「<a href='../secret/secretdgn535-585.html'>名も無き遺跡のデーモンキングのねぐら</a>」"],
+    309: ["秘密ダンジョン「<a href='../secret/secretdgn450-500.html'>スパインホールのミステリーゾーン</a>」"],
     319: ["秘密ダンジョン「<a href='../secret/secretdgn550-600.html'>ヘソパルの洞窟</a>」"],
     325: ["秘密ダンジョン「<a href='../secret/secretdgn600-640.html'>ビックマウスダンジョンのラットキング区域</a>」"],
     326: ["秘密ダンジョン「<a href='../secret/secretdgn600-640.html'>ビックマウスダンジョンのラットキング区域</a>」"],
+    337: ["秘密ダンジョン「<a href='../secret/secretdgn440-490.html'>ダークエルフ王宮の隠された倉庫</a>」"],
+    358: ["秘密ダンジョン「<a href='../secret/secretdgn405-435.html'>カダーム・ギガスの寺院</a>」"],
     382: ["秘密ダンジョン「<a href='../secret/secretdgn580-620.html'>モリネルタワーの隠された研究所</a>」"],
     383: ["秘密ダンジョン「<a href='../secret/secretdgn680-620.html'>モリネルタワーの隠された研究所</a>」"],
+    385: ["秘密ダンジョン「<a href='../secret/secretdgn355-405.html'>フォーリン望楼の隠された地下監獄</a>」"],
     450: ["秘密ダンジョン「<a href='../secret/secretdgn900-975.html'>逃亡者たちの山</a>」"],
     471: ["秘密ダンジョン「<a href='../secret/secretdgn651-680.html'>ヴァンパイア・キングダム</a>」"],
     474: ["秘密ダンジョン「<a href='../secret/secretdgn681-710.html'>ヴァンパイア・キングダム</a>」"],
