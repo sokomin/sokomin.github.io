fed6057b08a67ec1d656cf0aa4fc4a6c0b5aac91
sokomin <sokoranominsyu@gmail.com>
2021-09-05 07:16:04 +0900

    雛形

--- calculation/js/item/item-simulator.js
+++ calculation/js/item/item-simulator.js
@@ -1,34 +1,164 @@
 function getItemJson() {
     var req = new XMLHttpRequest();
     req.open("get", "https://sokomin.github.io/calculation/js/item/itemData.json", true);
+    req.overrideMimeType('text/plain; charset=Shift_JIS');
     req.send(null);
     req.onload = function () {
-        convertCSVtoArray(skill_str, req.responseText);
+        convertJson(req.responseText);
     }
 }
 
 function init1() {
-    var d1dom = document.getElementById("sub_d1");
-    var d2dom = document.getElementById("sub_d2");
-    var d3dom = document.getElementById("sub_d3");
-    // var d2dom = document.getElementByClassName("d2");
-    // var d3dom = document.getElementByClassName("d3");
-    skillIdMap.forEach(function (e, index) {
-        document.optionlist.a2.options[index] = new Option(e.value, e.value);
-        // .options[index] = new Option(e.value, e.value);
-        var element1 = document.createElement('option');
-        element1.setAttribute('value', e.value);
-        element1.innerHTML = e.value;
-        d1dom.appendChild(element1);
-        var element2 = document.createElement('option');
-        element2.setAttribute('value', e.value);
-        element2.innerHTML = e.value;
-        d2dom.appendChild(element2);
-        var element3 = document.createElement('option');
-        element3.setAttribute('value', e.value);
-        element3.innerHTML = e.value;
-        d3dom.appendChild(element3);
-        // document.keisannormal.d2.options[index] = new Option(e.value, e.value);
-        // document.keisannormal.d3.options[index] = new Option(e.value, e.value);
+    getItemJson();
+}
+
+var item_data = {};
+const chara_code = {
+    // 0: { name: "アイテム着用レベル -[数値]", },
+    0: { name: "剣士", },
+    1: { name: "戦士", },
+    2: { name: "ウィザード", },
+    3: { name: "ウルフマン", },
+    4: { name: "ビショップ", },
+    5: { name: "追放天使", },
+    6: { name: "シーフ", },
+    7: { name: "武道家", },
+    8: { name: "ランサー", },
+    9: { name: "アーチャー", },
+    10: { name: "ビーストテイマー", },
+    11: { name: "サマナー", },
+    12: { name: "プリンセス", },
+    13: { name: "リトルウィッチ", },
+    14: { name: "ネクロマンサー", },
+    15: { name: "悪魔", },
+    16: { name: "霊術師", },
+    17: { name: "闘士", },
+    18: { name: "光奏師", },
+    19: { name: "獣人", },
+    20: { name: "メイド", },
+    21: { name: "黒魔術師", },
+    22: { name: "マスケッティア", },
+    23: { name: "アルケミスト", },
+};
+
+
+function convertJson(itemJson) {
+    item_data = JSON.parse(itemJson);
+    console.log(item_data);
+    var j0dom = document.getElementById("jobtype");
+    var d0dom = document.getElementById("sub_0");
+    var d1dom = document.getElementById("sub_1");
+    var d2dom = document.getElementById("sub_2");
+    var d3dom = document.getElementById("sub_3");
+    var d4dom = document.getElementById("sub_4");
+    var d5dom = document.getElementById("sub_5");
+    var d6dom = document.getElementById("sub_6");
+    var d7dom = document.getElementById("sub_7");
+    var d8dom = document.getElementById("sub_8");
+    var d9_1dom = document.getElementById("sub_9_1");
+    var d9_2dom = document.getElementById("sub_9_2");
+    var d9_3dom = document.getElementById("sub_9_3");
+    var d9_4dom = document.getElementById("sub_9_4");
+    var d9_5dom = document.getElementById("sub_9_5");
+    var d9_6dom = document.getElementById("sub_9_6");
+    var d9_7dom = document.getElementById("sub_9_7");
+    var d9_8dom = document.getElementById("sub_9_8");
+    var d9_9dom = document.getElementById("sub_9_9");
+    var d9_10dom = document.getElementById("sub_9_10");
+    for(var index in chara_code){
+        var elements = document.createElement('option');
+        elements.setAttribute('value', index);
+        elements.innerHTML = chara_code[index].name;
+        j0dom.appendChild(elements);
+    };
+
+    item_data.forEach(function (e, index) {
+        // 初手では剣士のデータを出力
+        // document.optionlist.a2.options[index] = new Option(e.Type, e.Id);
+        let elements = document.createElement('option');
+        elements.setAttribute('value', e.Id);
+        elements.innerHTML = e.Name;
+        switch (e.Type) {
+            case 18:
+            // case 19:以降はタブ変わったら出す
+                d0dom.appendChild(elements);
+                break;
+            // case 12: タブ変わったら表示
+            // case 13:
+            // case 14:
+            // case 15:
+            case 19:
+                d1dom.appendChild(elements);
+                break;
+            case 8:
+                d2dom.appendChild(elements);
+                break;
+            case 0:
+            case 1:
+                d3dom.appendChild(elements);
+                break;
+            case 10:
+            case 11:
+                d4dom.appendChild(elements);
+                break;
+            case 6:
+                d5dom.appendChild(elements);
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                d6dom.appendChild(elements);
+                break;
+            case 16:
+            case 17:
+                d7dom.appendChild(elements);
+                break;
+            case 7:
+                d8dom.appendChild(elements);
+                break;
+            case 9:
+                // FIXME cloneってどうやるんだっけ
+                d9_1dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_2dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_3dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_4dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_5dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_6dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_7dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_8dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_9dom.appendChild(elements);
+                elements = document.createElement('option');
+                elements.setAttribute('value', e.Id);
+                elements.innerHTML = e.Name;
+                d9_10dom.appendChild(elements);
+                break;
+            default:
+                break;
+        }
     });
 }
