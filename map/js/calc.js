fce958631eb32427e3814e431e4d610fe2e3c6bb
sokomin <sokoranominsyu@gmail.com>
2022-03-27 10:56:07 +0900

    イラコンアイテム追加

--- map/js/calc.js
+++ map/js/calc.js
@@ -19,7 +19,8 @@ function getMap2CSV() {
 //モンスターDBも読まないと動かないので・・・重いけど全部読む
 function getMonsterCSV(mapCSV) {
     var req = new XMLHttpRequest();
-    req.open("get", "https://sokomin.github.io/update/js/monster.csv", true);
+    // req.open("get", "https://sokomin.github.io/update/js/monster.csv", true);
+    req.open("get", "https://sokomin.github.io/sokomin_repository/db/monster.csv", true);
     req.send(null);
     req.onload = function () {
         convertCSVtoArray(mapCSV, req.responseText);// 渡されるのは読み込んだCSVデータ
