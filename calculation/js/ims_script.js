2ded83534eb1dc5cb17589f5019d1e4f33db6407
sokomin <sokoranominsyu@gmail.com>
2023-10-15 21:44:40 +0900

    改修

--- calculation/js/ims_script.js
+++ calculation/js/ims_script.js
@@ -1,7 +1,8 @@
 // 画像のパスとファイル名の形式に基づいて、画像を動的に読み込む
-async function loadSampleImages() {
+async function loadSampleImages(fi, ti) {
     const sampleImages = [];
-    for (let i = 0; i < 4000; i++) {
+    for (let i = fi; i < ti; i++) {
+        // const filename = `https://sokomin.github.io/item/design/image/item/iconItem_${String(i).padStart(4, '0')}.png`;
         const filename = `../item/design/image/item/iconItem_${String(i).padStart(4, '0')}.png`;
         const img = new Image();
         img.crossOrigin = "anonymous";  // CORSポリシーを適用
@@ -38,50 +39,62 @@ async function findMatchingImage() {
     // ローディングマークを表示
     try {
         document.getElementById('loading').classList.remove('hidden');
+        let highestSimilarity = 0;
+        let mostSimilarImage;
+        let most_i = 0
 
-        const sampleImages = await loadSampleImages();
-        const imageInput = document.getElementById('imageInput');
-        const file = imageInput.files[0];
-        const reader = new FileReader();
+        for (var j = 0; j < 45; j++) {
+            let fi=j*100
+            let ti=j*100+99
+            const sampleImages = await loadSampleImages(fi, ti);
+            const imageInput = document.getElementById('imageInput');
+            const file = imageInput.files[0];
+            const reader = new FileReader();
 
-        reader.onload = function (e) {
-            const img = new Image();
-            img.src = e.target.result;
+            reader.onload = function (e) {
+                const img = new Image();
+                img.src = e.target.result;
 
-            img.onload = function () {
-                const canvas = document.createElement('canvas');
-                const ctx = canvas.getContext('2d');
-                canvas.width = 34;
-                canvas.height = 34;
+                img.onload = function () {
+                    const canvas = document.createElement('canvas');
+                    const ctx = canvas.getContext('2d');
+                    canvas.width = 34;
+                    canvas.height = 34;
 
-                ctx.drawImage(img, 0, 0, 34, 34);
-                const imgData1 = ctx.getImageData(0, 0, 34, 34);
+                    ctx.drawImage(img, 0, 0, 34, 34);
+                    const imgData1 = ctx.getImageData(0, 0, 34, 34);
 
-                // const sampleImages = document.getElementsByClassName('sample-image');
-                let highestSimilarity = 0;
-                let mostSimilarImage;
+                    // const sampleImages = document.getElementsByClassName('sample-image');
+                    // let highestSimilarity = 0;
 
-                i = 0
-                most_i = 0
-                for (const sampleImg of sampleImages) {
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.drawImage(sampleImg, 0, 0, 34, 34);
-                    const imgData2 = ctx.getImageData(0, 0, 34, 34);
+                    let il = 0
+                    for (const sampleImg of sampleImages) {
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        ctx.drawImage(sampleImg, 0, 0, 34, 34);
+                        const imgData2 = ctx.getImageData(0, 0, 34, 34);
 
-                    const similarity = calculatePixelSimilarity(imgData1, imgData2);
-                    if (similarity > highestSimilarity) {
-                        highestSimilarity = similarity;
-                        mostSimilarImage = sampleImg;
-                        most_i = i
+                        const similarity = calculatePixelSimilarity(imgData1, imgData2);
+                        if (similarity > highestSimilarity) {
+                            highestSimilarity = similarity;
+                            mostSimilarImage = sampleImg;
+                            most_i = fi+il
+                        }
+                        il++;
+                        if (highestSimilarity > 99.5) {
+                            break;
+                        }
                     }
-                    i++;
-                }
-
-                document.getElementById('result').innerText = `Most similar image found with ${highestSimilarity.toFixed(2)}% similarity. (image:${most_i})`;
-                document.getElementById('result').appendChild(mostSimilarImage.cloneNode());
+                };
             };
-        };
-        reader.readAsDataURL(file);
+            console.log(`${fi}: Most similar image found with ${highestSimilarity.toFixed(2)}% similarity. (image:${most_i})`)
+            reader.readAsDataURL(file);
+            if (highestSimilarity > 99.5) {
+                break;
+            }
+        }
+        document.getElementById('result').innerText = `一致度 ${highestSimilarity.toFixed(2)}% の画像が見つかりました。 (image_id:${String(most_i).padStart(4, '0')})`;
+        document.getElementById('result').appendChild(mostSimilarImage.cloneNode());
+
     } catch (error) {
         // エラーメッセージを表示
         console.error('An error occurred:', error);
